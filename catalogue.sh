884a8ac28591c4c2caffc07cfb6c26ee3b90b440
siva3150 <siva123@gmail.com>
2023-12-15 16:49:33 +0530

    roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.daws76s.online
+MONGDB_HOST=mongodb.sivadevops.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -42,7 +42,7 @@ dnf install nodejs -y  &>> $LOGFILE
 
 VALIDATE $? "Installing NodeJS:18"
 
-id roboshop
+id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
     useradd roboshop
@@ -97,10 +97,6 @@ VALIDATE $? "Installing MongoDB client"
 mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"
-
-
-
-
 
 
 
